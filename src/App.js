5bd88862685c36c9ceaf4897cdd12b38e1c362df
Dragan Smiljanic <dragan@myhometouch.com>
2017-04-20 10:56:33 +0200

    Set application layout

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ import './styling/styles.global.less'
 
 
 import Header from './components/Header';
+import SideBar from './components/SideBar';
 import './styling/styles.global.less'
-
+import styles from './App.less';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="container">
-          <Header current={80000} total={100000} />
+        <div className={styles.root}>
+          <div className={styles.sideBarWrapper}>
+            <SideBar />
+          </div>
+          <div className={styles.contentWrapper}>
+            <Header current={80000} total={100000}/>
+          </div>
         </div>
       </Provider>
     );
