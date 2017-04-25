788e55e663ed54bf8082565723a8fc25762ded64
Dragan Smiljanic <dragan@myhometouch.com>
2017-04-25 15:31:20 +0200

    Add waypoints redux stuff

---
+++ src/constants/actionTypes/index.js
@@ -0,0 +1,5 @@
+import * as waypointsActionType from './waypoints';
+
+export {
+  waypointsActionType,
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import waypointsReducer from './waypoints';
+
 export default function createReducer(asyncReducers) {
   return combineReducers({
     routing: routerReducer,
+    waypoints: waypointsReducer,
     ...asyncReducers
   });
 }
