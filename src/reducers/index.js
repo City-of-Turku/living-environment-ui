c227ed2337d607269513c44b03be32ffda82a83f
Dragan Smiljanic <dragan@myhometouch.com>
2017-04-25 18:17:53 +0200

    Initialize redux-form library

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form';
 
 import waypointsReducer from './waypoints';
 
 export default function createReducer(asyncReducers) {
   return combineReducers({
     routing: routerReducer,
+    formReducer,
     waypoints: waypointsReducer,
     ...asyncReducers
   });
