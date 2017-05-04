ab2c44222f2adfdda06b751ceadd3299b45a3785
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-04 18:40:47 +0200

    Add application header to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import PropType from 'prop-types';
+import classnames from 'classnames';
 
-import Header from './Header';
+import Header from '../containers/Header';
 import SideBar from '../containers/SideBar';
 
 import '../styling/styles.global.less';
 import styles from './Layout.less';
 
-const Layout = ({ children }) => (
+const Layout = ({ children, sideMenuShown }) => (
   <div className={styles.root}>
-    <div className={styles.sideBarWrapper}>
+    <Header />
+    <div className={classnames(styles.sideBarWrapper, sideMenuShown ? styles.sideMenuShown : '')}>
       <SideBar />
     </div>
     <div className={styles.contentWrapper}>
-      <Header moneyUsed={80000} totalBudget={100000} />
       {children}
     </div>
   </div>);
 
 Layout.propTypes = {
   children: PropType.node.isRequired,
+  sideMenuShown: PropType.bool.isRequired,
 };
 
 export default Layout;
