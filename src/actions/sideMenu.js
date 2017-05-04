479eb943e5a38b730dd4c1e41ef89cf0d689929e
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-04 18:35:29 +0200

    Wire the application header component to store
    * add sideMenu action creator and type
    * add sideMenu reducer

---
+++ src/actions/sideMenu.js
@@ -0,0 +1,7 @@
+import { sideMenuActionType } from '../constants/actionTypes';
+
+export const toggleMenu = () => ({
+  type: sideMenuActionType.TOGGLE_MENU,
+});
+
+export default toggleMenu;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
 
+import sideMenuReducer from './sideMenu';
 import waypointsReducer from './waypoints';
 
 export default function createReducer(asyncReducers) {
   return combineReducers({
     formReducer,
     routing: routerReducer,
+    sideMenu: sideMenuReducer,
     waypoints: waypointsReducer,
     ...asyncReducers
   });
