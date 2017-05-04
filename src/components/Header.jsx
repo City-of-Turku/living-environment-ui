e163c7e637d375ca9dcc3d02bd8cc5bf743478b9
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-04 18:02:06 +0200

    Create the application header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import PropType from 'prop-types';
-import { Row, OverlayTrigger, ProgressBar, Tooltip } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import classnames from 'classnames';
 
 import styles from './Header.less';
 
-const calcPercent = (moneyUsed, totalBudget) => 100 * (moneyUsed / totalBudget);
 
-const Header = ({ moneyUsed, totalBudget }) => (
+const Header = ({ toggleMenu }) => (
   <Row className={styles.root}>
-    <div className={styles.label}>Assignment Name</div>
-    <div className={styles.progressBarWrapper}>
-      <OverlayTrigger
-        placement="bottom"
-        overlay={
-          <Tooltip id="progress" className="success">
-            {`${moneyUsed}/${totalBudget}`}
-          </Tooltip>
-        }
-      >
-        <ProgressBar now={calcPercent(moneyUsed, totalBudget)} bsStyle="success" />
-      </OverlayTrigger>
+    <div className={styles.contentWrapper}>
+      <div className={styles.toggleButtonWrapper}>
+        <button className={classnames('fa fa-bars', styles.button)} onClick={toggleMenu} role="button" />
+      </div>
+      <div className={styles.logoWrapper}>
+        <i className={classnames('glyphfont', 'turku-logo', styles.logo)} />
+      </div>
     </div>
   </Row>);
 
 Header.propTypes = {
-  moneyUsed: PropType.number.isRequired,
-  totalBudget: PropType.number.isRequired,
+  toggleMenu: PropType.func.isRequired,
 };
 
 export default Header;
