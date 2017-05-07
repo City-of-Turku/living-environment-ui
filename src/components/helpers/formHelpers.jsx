e5b158acea2f60a0e19a85c77e9f21049a248256
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-07 04:27:56 +0200

    Add  input field to redux-form field renderer

--- src/components/helpers/formHelpers.jsx
+++ src/components/helpers/formHelpers.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const renderField = (field) => { // eslint-disable-line import/prefer-default-export
+  const placeholder = field.placeholder || field.label;
+  // all input types except textarea and select
+  const otherTypes = field.type !== 'textarea' && field.type !== 'select';
   const attributes = {
     ...field.input,
-    placeholder: field.placeholder || field.label,
+    placeholder,
     type: field.type,
     className: 'form-control',
   };
@@ -13,7 +16,21 @@ export const renderField = (field) => { // eslint-disable-line import/prefer-def
     <div>
       { field.label && <label htmlFor={name}>{field.label}</label> }
       <div>
-        { field.type ? <textarea {...attributes} /> : <input {...attributes} /> }
+        { field.type === 'textarea' && <textarea {...attributes} /> }
+        { field.type === 'select' &&
+          <select {...attributes}>
+            {
+              placeholder && <option key={placeholder}>{placeholder}</option>
+            }
+            {
+              field.options.map(option => (
+                <option key={option[field.optionsKey]} value={option[field.optionsKey]}>
+                  {option[field.optionsValue]}
+                </option>))
+            }
+          </select>
+        }
+        { otherTypes && <input {...attributes} /> }
       </div>
     </div>
   );
