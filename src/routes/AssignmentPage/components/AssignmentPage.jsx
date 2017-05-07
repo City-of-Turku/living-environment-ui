1dc586e7e1ea30d34659bdc184993450905ce3c9
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-07 04:29:33 +0200

    Add player information section to assignment page

--- src/routes/AssignmentPage/components/AssignmentPage.jsx
+++ src/routes/AssignmentPage/components/AssignmentPage.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import ContentWrapper from '../../../containers/ContentWrapper';
 import Header from './Header';
+import PlayerInformation from './PlayerInformation';
 import OpenTextTask from './OpenTextTask';
 import TaskInfoBar from './TaskInfoBar';
 import TaskContent from './TaskContent';
@@ -29,32 +30,37 @@ class AssignmentPage extends Component {
       return null;
     }
     return (<section>
-      <ContentWrapper id="_" />
-      <div className={styles.root}>
-        <Header moneyUsed={3} totalBudget={5} />
-        <ContentWrapper id={`${assignment.id}-assignment`}>
-          <div className={styles.heroImageWrapper}>
-            <img
-              className={styles.heroImage}
-              src="http://placehold.it/750x250"
-              alt=""
+      <form name="assignmentPage">
+        <ContentWrapper id="_" />
+        <div className={styles.root}>
+          <Header moneyUsed={3} totalBudget={5} />
+          <ContentWrapper id={`${assignment.id}-assignment`}>
+            <div className={styles.heroImageWrapper}>
+              <img
+                className={styles.heroImage}
+                src="http://placehold.it/750x250"
+                alt=""
+              />
+            </div>
+            <TaskInfoBar
+              categoryName={'Example Category'}
+              tasks={{ completed: 10, total: 20 }}
+              totalBudget={100000}
             />
-          </div>
-          <TaskInfoBar
-            categoryName={'Example Category'}
-            tasks={{ completed: 10, total: 20 }}
-            totalBudget={100000}
-          />
-          <TaskContent
-            body={assignment.description}
-            title={assignment.name}
-          />
-        </ContentWrapper>
-        {
-          assignment.sections && assignment.sections.map(
-            section => this.openTextTasks(assignment.id, section))
-        }
-      </div>
+            <TaskContent
+              body={assignment.description}
+              title={assignment.name}
+            />
+          </ContentWrapper>
+          <ContentWrapper id={`${assignment.id}-assignment-my-information`}>
+            <PlayerInformation schools={assignment.schools} />
+          </ContentWrapper>
+          {
+            assignment.sections && assignment.sections.map(
+              section => this.openTextTasks(assignment.id, section))
+          }
+        </div>
+      </form>
     </section>);
   }
 }
