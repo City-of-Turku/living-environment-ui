66777925a0da96e7bb749d7fd160dd4ccf67774a
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-09 15:31:36 +0200

    Update OpenTextTask component
    * remove placeholder
    * adopt to changes of API response

--- src/routes/AssignmentPage/components/OpenTextTask.jsx
+++ src/routes/AssignmentPage/components/OpenTextTask.jsx
@@ -7,21 +7,21 @@ import PropTypes from 'prop-types';
 import styles from './OpenTextTask.less';
 import { renderField } from '../../../components/helpers/formHelpers';
 
-const OpenTextTask = ({ id, question }) => (
+const OpenTextTask = ({ id, data }) => (
   <div>
     <Row>
       <Col xs={12} sm={2} className={styles.iconWrapper}>
         <i className={classNames('glyphfont', 'icon-edit', styles.editIcon)} />
       </Col>
       <Col xs={12} sm={8}>
-        {question}
+        {data.question}
       </Col>
     </Row>
     <Row>
       <Col xs={12} sm={8} smOffset={2} className={classNames('form-group', styles.inputFieldWrapper)}>
         <Field
           name={`open_text_task_${id}`}
-          placeholder="Define placeholder HERE..."
+          placeholder=""
           component={renderField} type="textarea"
         />
       </Col>
@@ -31,7 +31,10 @@ const OpenTextTask = ({ id, question }) => (
 
 OpenTextTask.propTypes = {
   id: PropTypes.number.isRequired,
-  question: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    question: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 
