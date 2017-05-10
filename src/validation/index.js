08ac284d4e8b6e9c01a3d4aee1822932ee851b25
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-10 21:58:34 +0200

    Add numeric validators
    * the validator `number` checks whether the given string represent a number
    * the validator `range` check whether the given string represent a number in the given range

---
+++ src/validation/index.js
@@ -0,0 +1,19 @@
+export const number = value => (value && isNaN(Number(value)) ? 'A number is expected' : undefined);
+
+export const range = (value, min, max) => { // eslint-disable-line import/prefer-default-export
+  const minDefined = typeof min === 'number';
+  const maxDefined = typeof max === 'number';
+  if (minDefined && maxDefined) {
+    if (min > max) {
+      return console.warn(`min value ${min} can't be greater then max value ${max}`); // eslint-disable-line
+    }
+    if (value < min || value > max) {
+      return `Arvon on oltava väliltä ${min} ja ${max}`; // The value should't be less than ${min}
+    }
+  } else if (minDefined && value < min) {
+    return `Arvo ei saa olla pienempi kuin ${min}`; // The value should't be less than ${min}
+  } else if (maxDefined && value > max) {
+    return `'Arvo ei voi olla suurempi kuin ${max}'`; // The value should't be greater than ${max}
+  }
+  return undefined; // value is valid as min and max limits are not defined
+};
