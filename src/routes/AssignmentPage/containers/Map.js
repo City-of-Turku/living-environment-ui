532356857e2f0c5b07e3fbfe02f88cb233c1bb96
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-19 15:27:13 +0200

    Update map component

---
+++ src/routes/AssignmentPage/containers/Map.js
@@ -0,0 +1,22 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import Map from '../components/Map';
+import { addTarget, budgetingMapClicked, cleanInvalidTarget,
+  deleteTarget, updateTarget } from '../actions/budgetingMap';
+
+const mapStateToProps = (state, ownProps) => ({
+  maskPolygon: ownProps.maskPolygon,
+  targetUserData: state.budgetingMap.tasks[ownProps.task.id].targetUserData,
+});
+
+const mapDispatchToProps = (dispatch, ownProps) =>
+  bindActionCreators({
+    handleAddTarget: selectedTarget => addTarget(ownProps.task.id, selectedTarget),
+    handleBudgetingMapTargetListDialogClosed: () => cleanInvalidTarget(ownProps.task.id),
+    handleDeleteTarget: (targetId, selectedTarget) => deleteTarget(ownProps.task.id, targetId, selectedTarget),
+    handleUpdateTarget: (targetId, selectedTarget) => updateTarget(ownProps.task.id, targetId, selectedTarget),
+    handleMapClick: (lat, lng, task) => budgetingMapClicked(lat, lng, task.id),
+  }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Map);
