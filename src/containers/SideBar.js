6672e503b1d563074dcd878fb43a617519b0f0fe
Dragan Smiljanic <dragan@myhometouch.com>
2017-05-27 01:09:57 +0200

    Show spent budget in side menu
    * async react-router's url pattern matching is replaced to route-pattern  as react-router's matching fires reload of the route on each change of the app state
    * calculated budget total, spent budget for all sections and spent budget per section
    * shown a badge pre menu item in menu in order to represent spent budget per section

--- src/containers/SideBar.js
+++ src/containers/SideBar.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 
 import SideBar from '../components/SideBar';
+import calcAssignmentBudget from '../components/helpers/budgetingHelper';
 
 const mapStateToProps = state => ({
   currentSection: state.waypoints.currentSection,
   assignment: state.assignment.assignment,
+  budget: calcAssignmentBudget(state),
 });
 
 export default connect(mapStateToProps)(SideBar);
