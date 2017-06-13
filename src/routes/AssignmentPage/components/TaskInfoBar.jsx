9c91b358f66b5ca0e6dc2d52f8bd004f9b666f2f
Tomi Järvi <tomi@jarvi.pro>
2017-06-13 20:30:34 +0300

    Add euro to all currency fields

--- src/routes/AssignmentPage/components/TaskInfoBar.jsx
+++ src/routes/AssignmentPage/components/TaskInfoBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Col, Row } from 'react-bootstrap';
 import classNames from 'classnames';
+import currencyFormatter from 'currency-formatter';
 
 import styles from './TaskInfoBar.less';
 
@@ -9,11 +10,11 @@ const TaskInfoBar = ({ tasksCount, totalBudget }) => (
   <div className={styles.root}>
     <Row>
       <Col xs={12} sm={6} className={classNames(styles.col, styles.firstCol)}>
-        <div>Total Budget</div>
-        <div className={styles.value}>{totalBudget}</div>
+        <div>Budjetti</div>
+        <div className={styles.value}>{currencyFormatter.format(totalBudget, { locale: 'fi-FI' })}</div>
       </Col>
       <Col xs={12} sm={6} className={styles.col}>
-        <div>Tasks Count</div>
+        <div>Tehtävien määrä</div>
         <div className={styles.value}>{tasksCount}</div>
       </Col>
     </Row>
