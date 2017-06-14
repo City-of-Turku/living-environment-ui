18f91d0c2e2018b39157901027d627411d4e4e53
Dragan Smiljanic <dragan@myhometouch.com>
2017-06-14 16:32:36 +0200

    Set initial values of form fields for the budgeting text tasks

---
+++ src/reducers/assignmentFormReducer.js
@@ -0,0 +1,47 @@
+import { reducer as formReducer } from 'redux-form';
+
+import * as assignmentActionType from '../routes/AssignmentPage/constants/actionTypes/assignment';
+import * as TaskType from '../constants/taskTypes/index';
+
+const calculateFormInitialValues = (assignment) => {
+  const formInitialValues = {};
+  ((assignment || {}).sections || []).forEach((section) => {
+    (section.tasks || []).forEach((task) => {
+      if (task.task_type === TaskType.BudgetingTask && task.data.budgeting_type === TaskType.BudgetingTextTask) {
+        (task.data.targets || []).forEach((target) => {
+          const name = `budgeting_text_task_${task.id}_${target.id}`;
+          formInitialValues[name] = target.reference_amount;
+        });
+      }
+    });
+  });
+  return formInitialValues;
+};
+
+const assignmentFormReducer = {
+  // ... your other reducers here ...
+  form: formReducer.plugin({
+    assignmentPage: (state, action) => {   // <----- 'login' is name of form given to reduxForm()
+      switch (action.type) {
+      case assignmentActionType.FETCH_ASSIGNMENT_FULFILLED:
+        const assignment = action.payload;
+        const initialValues = calculateFormInitialValues(assignment);
+        debugger;
+        return {
+          ...state,
+          values: {
+            ...state.values,
+            ...initialValues,
+          },
+          fields: {
+            ...state.fields,
+          }
+        };
+      default:
+        return state;
+      }
+    }
+  })
+};
+
+export default assignmentFormReducer;
